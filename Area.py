141848aebb190be670d14787bd7f9779456b693d
Arnesh Rajendran <72863152+Arnesha4@users.noreply.github.com>
2023-05-24 10:24:44 +0530

    Add files via upload

---
+++ Area.py
@@ -0,0 +1,8 @@
+import math
+print('Hello user!')
+rad=float(input("Enter radius:"))
+vol=(4/3)*(3.14)*(rad**3)
+print("Volume of sphere is ",vol,"units")
+sa=4*(3.14)*(rad**2)
+print("Surface area of sphere is ",sa,"units")
+    
